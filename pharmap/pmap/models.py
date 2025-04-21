ffb83efa46348379a5e410c636d6c3f3bdd5e46f
han <162548361+hanhan39@users.noreply.github.com>
2025-04-21 16:24:12 +0800

    create models

--- pharmap/pmap/models.py
+++ pharmap/pmap/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=50)
+    branch = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name} - {self.branch}'
+
+class Medicine(models.Model):
+    brand = models.CharField(max_length=50)
+    generic = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.brand} ({self.generic}) - PHP {self.price}'
+
+class InventoryMed(models.Model):
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE, null=False)
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE, null=False)
+
+class MedList(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    created_at = models.DateTimeField()
+    def __str__(self):
+        return f'{self.name} ({self.user}) - {self.created_at}'
+
+class ListItem(models.Model):
+    medlist = models.ForeignKey(MedList, on_delete=models.CASCADE, null=False)
+    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE, null=False)
