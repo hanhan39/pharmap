00a99934ea5c87f6021567dee9a42d4f0bf61fdf
han <162548361+hanhan39@users.noreply.github.com>
2025-04-21 20:06:36 +0800

    add dummy data, revise model

--- pharmap/pmap/admin.py
+++ pharmap/pmap/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pharmacy, Medicine, InventoryMed, MedList, ListItem
 
 # Register your models here.
+admin.site.register(Pharmacy)
+admin.site.register(Medicine)
+admin.site.register(InventoryMed)
+admin.site.register(MedList)
+admin.site.register(ListItem)
--- pharmap/pmap/models.py
+++ pharmap/pmap/models.py
@@ -12,23 +12,30 @@ class Pharmacy(models.Model):
 
 class Medicine(models.Model):
     brand = models.CharField(max_length=50)
-    generic = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    generic = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.brand} ({self.generic}) - PHP {self.price}'
+        return f'{self.brand} ({self.generic})'
 
 class InventoryMed(models.Model):
     pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE, null=False)
     medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE, null=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.pharmacy}: {self.medicine} - PHP {self.price}'
 
 class MedList(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     created_at = models.DateTimeField()
+
     def __str__(self):
         return f'{self.name} ({self.user}) - {self.created_at}'
 
 class ListItem(models.Model):
     medlist = models.ForeignKey(MedList, on_delete=models.CASCADE, null=False)
     medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return f'{self.medlist}: {self.medicine}'
